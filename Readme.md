e08db902a65aaac885b915195adf6b8aa3423146
fraolb <fraolbereket@gmail.com>
2024-08-04 22:45:22 +0300

    initialize smart contract

--- Readme.md
+++ Readme.md
@@ -1,1 +1,1 @@
-This is a crosschain defi project
+This is a crosschain defi project. It helps users to borrow tokens in one chain while having collateral in other chain.
--- smartcontract/src/Counter.sol
+++ smartcontract/src/Counter.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.13;
 contract Counter {
     uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    function setNumber(uint256 newNumber1) public {
+        number = newNumber1;
     }
 
     function increment() public {
