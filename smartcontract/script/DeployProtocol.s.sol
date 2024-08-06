22251a963b78e687aeb9ef2da45e44591512f041
fraolb <fraolbereket@gmail.com>
2024-08-07 00:50:58 +0300

    fix the deploy contract

--- smartcontract/script/DeployProtocol.s.sol
+++ smartcontract/script/DeployProtocol.s.sol
@@ -2,3 +2,29 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {ProtocolConfig} from "./ProtocolConfig.s.sol";
+import {LendProtocol} from "../src/LendProtocol.sol";
+
+contract DeployProtocol is Script {
+    struct NetworkConfig {
+        address router;
+        uint256 chainSelector;
+        address[] tokens;
+        address[] tokenPriceFeeds;
+    }
+
+    function run() external returns (LendProtocol, ProtocolConfig) {
+        ProtocolConfig config = new ProtocolConfig();
+        ProtocolConfig.NetworkConfig memory networkConfig = config.getActiveNetworkConfig();
+
+        vm.startBroadcast();
+
+        LendProtocol lendProtocol = new LendProtocol(
+            networkConfig.router, networkConfig.chainSelector, networkConfig.tokens, networkConfig.tokenPriceFeeds
+        );
+
+        vm.stopBroadcast();
+
+        return (lendProtocol, config);
+    }
+}
--- smartcontract/script/ProtocolConfig.s.sol
+++ smartcontract/script/ProtocolConfig.s.sol
@@ -10,10 +10,8 @@ contract ProtocolConfig is Script {
     struct NetworkConfig {
         address router;
         uint256 chainSelector;
-        address tokenOne;
-        address tokenTwo;
-        address tokenOnePriceFeed;
-        address tokenTwoPriceFeed;
+        address[] tokens;
+        address[] tokenPriceFeeds;
     }
 
     uint8 public constant DECIMALS = 8;
@@ -38,46 +36,70 @@ contract ProtocolConfig is Script {
     }
 
     function getBaseConfig() public view returns (NetworkConfig memory) {
+        address[] memory tokens = new address[](2);
+        tokens[0] = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
+        tokens[1] = 0xA98FA8A008371b9408195e52734b1768c0d1Cb5c;
+
+        address[] memory tokenPriceFeeds = new address[](2);
+        tokenPriceFeeds[0] = 0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165;
+        tokenPriceFeeds[1] = 0x3c65e28D357a37589e1C7C86044a9f44dDC17134;
+
         return NetworkConfig({
             router: 0xD3b06cEbF099CE7DA4AcCf578aaebFDBd6e88a93,
             chainSelector: 10344971235874465080,
-            tokenOne: 0x036CbD53842c5426634e7929541eC2318f3dCF7e,
-            tokenTwo: 0xA98FA8A008371b9408195e52734b1768c0d1Cb5c,
-            tokenOnePriceFeed: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
-            tokenTwoPriceFeed: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063
+            tokens: tokens,
+            tokenPriceFeeds: tokenPriceFeeds
         });
     }
 
     function getBaseTestnetConfig() public view returns (NetworkConfig memory) {
+        address[] memory tokens = new address[](2);
+        tokens[0] = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
+        tokens[1] = 0xA98FA8A008371b9408195e52734b1768c0d1Cb5c;
+
+        address[] memory tokenPriceFeeds = new address[](2);
+        tokenPriceFeeds[0] = 0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165;
+        tokenPriceFeeds[1] = 0x3c65e28D357a37589e1C7C86044a9f44dDC17134;
+
         return NetworkConfig({
             router: 0xD3b06cEbF099CE7DA4AcCf578aaebFDBd6e88a93,
             chainSelector: 10344971235874465080,
-            tokenOne: 0x036CbD53842c5426634e7929541eC2318f3dCF7e,
-            tokenTwo: 0xA98FA8A008371b9408195e52734b1768c0d1Cb5c,
-            tokenOnePriceFeed: 0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165,
-            tokenTwoPriceFeed: 0x3c65e28D357a37589e1C7C86044a9f44dDC17134
+            tokens: tokens,
+            tokenPriceFeeds: tokenPriceFeeds
         });
     }
 
     function getOptmisimConfig() public view returns (NetworkConfig memory) {
+        address[] memory tokens = new address[](2);
+        tokens[0] = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
+        tokens[1] = 0xA98FA8A008371b9408195e52734b1768c0d1Cb5c;
+
+        address[] memory tokenPriceFeeds = new address[](2);
+        tokenPriceFeeds[0] = 0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165;
+        tokenPriceFeeds[1] = 0x3c65e28D357a37589e1C7C86044a9f44dDC17134;
+
         return NetworkConfig({
             router: 0xD3b06cEbF099CE7DA4AcCf578aaebFDBd6e88a93,
             chainSelector: 10344971235874465080,
-            tokenOne: 0x036CbD53842c5426634e7929541eC2318f3dCF7e,
-            tokenTwo: 0xA98FA8A008371b9408195e52734b1768c0d1Cb5c,
-            tokenOnePriceFeed: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
-            tokenTwoPriceFeed: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063
+            tokens: tokens,
+            tokenPriceFeeds: tokenPriceFeeds
         });
     }
 
     function getOptmisimTestnetConfig() public view returns (NetworkConfig memory) {
+        address[] memory tokens = new address[](2);
+        tokens[0] = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
+        tokens[1] = 0xA98FA8A008371b9408195e52734b1768c0d1Cb5c;
+
+        address[] memory tokenPriceFeeds = new address[](2);
+        tokenPriceFeeds[0] = 0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165;
+        tokenPriceFeeds[1] = 0x3c65e28D357a37589e1C7C86044a9f44dDC17134;
+
         return NetworkConfig({
             router: 0xD3b06cEbF099CE7DA4AcCf578aaebFDBd6e88a93,
             chainSelector: 10344971235874465080,
-            tokenOne: 0x036CbD53842c5426634e7929541eC2318f3dCF7e,
-            tokenTwo: 0xA98FA8A008371b9408195e52734b1768c0d1Cb5c,
-            tokenOnePriceFeed: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
-            tokenTwoPriceFeed: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063
+            tokens: tokens,
+            tokenPriceFeeds: tokenPriceFeeds
         });
     }
 
@@ -92,13 +114,23 @@ contract ProtocolConfig is Script {
         ERC20Mock usdMock = new ERC20Mock("USD", "USD", msg.sender, 1000e8);
         vm.stopBroadcast();
 
+        address[] memory tokens = new address[](2);
+        tokens[0] = address(usdMock);
+        tokens[1] = address(wethMock);
+
+        address[] memory tokenPriceFeeds = new address[](2);
+        tokenPriceFeeds[0] = address(usdPriceFeed);
+        tokenPriceFeeds[1] = address(ethUsdPriceFeed);
+
         return NetworkConfig({
             router: 0xD3b06cEbF099CE7DA4AcCf578aaebFDBd6e88a93,
             chainSelector: 10344971235874465080,
-            tokenOne: address(usdMock),
-            tokenTwo: address(wethMock),
-            tokenOnePriceFeed: address(usdPriceFeed),
-            tokenTwoPriceFeed: address(ethUsdPriceFeed)
+            tokens: tokens,
+            tokenPriceFeeds: tokenPriceFeeds
         });
+    }
+
+    function getActiveNetworkConfig() public view returns (NetworkConfig memory) {
+        return activeNetworkConfig;
     }
 }
