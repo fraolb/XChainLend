ecf552c7a93a0609c510ef2d685736a7e79acf7e
fraolb <fraolbereket@gmail.com>
2024-08-09 13:05:05 +0300

    fix layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,7 +1,10 @@
+"use client";
 import { FC, ReactNode } from "react";
 import { ConnectButton } from "thirdweb/react";
+import { sepolia, baseSepolia, avalancheFuji } from "thirdweb/chains";
 import { client } from "@/app/client";
 import Link from "next/link";
+import { createWallet } from "thirdweb/wallets";
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,24 +13,31 @@ interface LayoutProps {
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="bg-primary text-white min-h-screen w-full">
-      <header className="p-4 bg-secondary shadow-lg border border-bottom-solid">
+      <header className="p-4 bg-secondary shadow-lg border-b border-solid">
         <div className="container mx-auto flex justify-between items-center">
           <h1 className="text-xl font-bold">XChain Lend</h1>
-          <div className="flex">
-            <Link href="#" className="ml-3 text-accent">
+          <div className="flex items-center space-x-4">
+            <Link href="#" className="text-accent">
               Home
             </Link>
-            <Link href="#" className="ml-3 text-accent">
+            <Link href="#" className="text-accent">
               Markets
             </Link>
-            <Link href="#" className="ml-3 text-accent">
+            <Link href="#" className="text-accent">
               Governance
             </Link>
-            <div className="ml-3 text-accent">
+            <div className="text-accent">
               <ConnectButton
                 client={client}
+                wallets={[
+                  createWallet("io.metamask"),
+                  createWallet("com.coinbase.wallet"),
+                  createWallet("me.rainbow"),
+                ]}
+                theme="light"
+                chains={[sepolia, baseSepolia, avalancheFuji]}
                 appMetadata={{
-                  name: "Example App",
+                  name: "XChain Lend",
                   url: "https://example.com",
                 }}
               />
